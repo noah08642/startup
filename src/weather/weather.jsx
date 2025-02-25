5a188ff538f3a91aab34192052de682ec05f2ecd
noah08642 <lukerichards8@gmail.com>
2025-02-25 14:54:52 -0700

    Added weather files

---
+++ src/weather/weather.jsx
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+import './weather.css';
+
+export function Resume() {
+  return (
+    <main>
+      <div className="resume-and-link">
+        <div className="resume-container">
+          <iframe src="resume.pdf" ></iframe>
+        </div>
+        <div className="resume-download">
+          <a href="resume.pdf" download> Download resume</a>
+        </div>        
+      </div>
+    </main>
+  );
+}
