98aaa9be83f8ed9921cca9cd37b8cdd2246e8efa
noah08642 <lukerichards8@gmail.com>
2025-03-09 23:13:27 -0600

    Added service boiler plate code

--- service/index.js
+++ service/index.js
@@ -1,7 +1,32 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
 const express = require('express');
+const uuid = require('uuid');
 const app = express();
 
+const authCookieName = 'token';
+
+// The scores and users are saved in memory and disappear whenever the service is restarted.
+let users = [];
+let scores = [];
+
+// The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
+// Serve up the front-end static content hosting
+app.use(express.static('public'));
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+
 
 app.get('*', (_req, res) => {
   res.send({ msg: 'Simon service' });
