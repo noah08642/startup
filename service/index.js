7f7880401556e1c131d91e3c2d2e9e1f3b8e0295
noah08642 <lukerichards8@gmail.com>
2025-03-12 13:11:37 -0600

    resolved deployment issues.  Still working through backend 404 error

--- service/index.js
+++ service/index.js
@@ -10,7 +10,7 @@ const authCookieName = 'token';
 let users = [];
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
@@ -23,11 +23,7 @@ app.use(express.static('public'));
 
 // Router for service endpoints
 var apiRouter = express.Router();
-app.use(`/api`, apiRouter);
-
-
-
-
+app.use('/api', apiRouter);
 
 // CreateAuth a new user
 apiRouter.post('/auth/create', async (req, res) => {
--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Button from 'react-bootstrap/Button';
-import { MessageDialog } from './messageDialog';
+import { MessageDialog } from './messageDialogue';
 
 export function Unauthenticated(props) {
   const [userName, setUserName] = React.useState(props.userName);
